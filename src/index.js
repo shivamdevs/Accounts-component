1777507877998149de521314f6ef19de3f5371b9
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2023-01-11 11:02:17 +0530

    Code update 4.a
    Compiled all in one folder.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Accounts from './components/Accounts/Accounts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <Routes>
+                <Route path="/accounts/*" element={<Accounts />} />
+                <Route exat path="/" element={<Navigate to="/accounts" />} />
+            </Routes>
         </BrowserRouter>
     </React.StrictMode>
 );
